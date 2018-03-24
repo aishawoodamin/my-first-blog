626f2a86998a8aae1a1e01523da5bc78da40b90d
aishawoodamin <aisha.woodamin@gmail.com>
2018-03-24 14:10:17 +0100

    add full url

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l!@7pwa4m0783s2(a9-+te9puopbxdruxas9m-9(d#768&a()y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'aishawoodamin.pythonanywhere.com']
 
 
 # Application definition
